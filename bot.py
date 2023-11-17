5b5fcd1293d25844495d51a4284dc53b7ac0ed28
mizerhit <mizerhit@gmail.com>
2023-11-17 14:28:58 +0300

    Added new function in bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import telebot
 from telebot import types
+# import menu
 
 token = "6398606477:AAEIIC64vT5WqQrgoDnaaIuc6NM_694F3RA"
 bot = telebot.TeleBot(token)
@@ -27,15 +28,16 @@ subject_mark = types.ReplyKeyboardMarkup(
 
 menu_btn1 = types.KeyboardButton("Расписание")
 menu_btn2 = types.KeyboardButton("Домашняя работа")
-menu_mark.add(menu_btn1, menu_btn2)
+menu_btn3 = types.KeyboardButton("Выбор группы")
+menu_mark.add(menu_btn1, menu_btn2, menu_btn3)
 
 homework_btn2 = types.KeyboardButton("Добавление дз")
 homework_btn1 = types.KeyboardButton("Просмотр дз")
 homework_mark.add(homework_btn1, homework_btn2)
 
 subject_btn2 = types.KeyboardButton("Выберите предмет")
-subject_btn1 = types.KeyboardButton("Дз на неделю")
-subject_mark.add(subject_btn1, subject_btn2)
+# subject_btn1 = types.KeyboardButton("Дз на неделю")
+subject_mark.add(subject_btn2)
 
 
 @bot.message_handler(commands=['start'])
@@ -48,8 +50,6 @@ def start(message):
 def get_text_messages(message):
     if message.text == "Расписание":
         bot.send_message(message.from_user.id, schedule)
-        print(message.from_user.id, message.from_user.first_name,
-              message.from_user.last_name, message.from_user.username)
 
     elif message.text == "/help":
         bot.send_message(message.from_user.id,
@@ -61,19 +61,25 @@ def get_text_messages(message):
 
     elif message.text == "Добавление дз":
         bot.send_message(message.from_user.id, "Введите ДЗ: Предмет-> дз")
+        bot.register_next_step_handler(message, get_homework)
 
     elif message.text == "Выбрать предмет":
         bot.send_message(message.from_user.id, "К какому предмету дз? ")
 
     elif message.text == "Выберите предмет":
-        bot.send_message(message.from_user.id, "Список предметов:")
+        bot.send_message(message.from_user.id, "Введите название предмета")
+        bot.register_next_step_handler(message, get_subject)
 
     elif message.text == "Просмотр дз":
-        bot.send_message(message.from_user.id, homework,
+        bot.send_message(message.from_user.id, "Какое дз хотите посмотреть?",
                          reply_markup=subject_mark)
 
-    elif message.text == "Дз на неделю":
-        bot.send_message(message.from_user.id, homework)
+    # elif message.text == "Дз на неделю":
+    #     bot.send_message(message.from_user.id, homework)
+
+    elif message.text == "Выбор группы":
+        bot.send_message(message.from_user.id, "Введите название вашей группы")
+        bot.register_next_step_handler(message, get_group)
 
     else:
         bot.send_message(message.from_user.id,
@@ -85,6 +91,22 @@ def get_homework(message):
     homework = message.text
     bot.send_message(message.from_user.id, "Домашняя работа добавленна",
                      reply_markup=menu_mark)
+
+
+def get_subject(message):
+    global subject
+    subject = message.text
+    bot.send_message(message.from_user.id, "Вот ДЗ",
+                     reply_markup=menu_mark)
+
+
+def get_group(message):
+    global group
+    group = message.text
+    bot.send_message(message.from_user.id, text="Группа добавлена",
+                     reply_markup=menu_mark)
+    bot.send_message(message.from_user.id, group)
+    print(message.from_user.username)
 
 
 bot.polling(none_stop=True, interval=0)
